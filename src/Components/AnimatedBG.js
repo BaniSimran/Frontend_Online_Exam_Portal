017007868513b10d275066cd8744aeca98755421
veloxigami <anil.kumar.iiitm@gmail.com>
2022-04-17 23:44:58 +0530

    frontend patch

---
+++ src/Components/AnimatedBG.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styling from '../Resources/bg.module.css';
+
+export default function AnimatedBG() {
+  return (
+    <div className={styling.area} >
+            <ul className={styling.circles}>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+    </div >
+  )
+}
